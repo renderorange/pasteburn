f7602cb4f01efc709dc122903c3bd89552cb3bf9
Blaine Motsinger <blaine@renderorange.com>
2020-08-24 23:10:43 -0500

    Add documentation for Hash

--- lib/Pasteburn/Crypt/Hash.pm
+++ lib/Pasteburn/Crypt/Hash.pm
@@ -53,3 +53,80 @@ sub validate {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+Pasteburn::Crypt::Hash - secure string hashing and validation
+
+=head1 SYNOPSIS
+
+ use Pasteburn::Crypt::Hash;
+
+ my $crypt = Pasteburn::Crypt::Hash->new();
+
+ my $hash  = $crypt->generate( string => $string );
+ my $valid = $crypt->validate( hash => $hash, string => $string );
+
+=head1 DESCRIPTION
+
+This module provides secure string validation and hashing for storing strings.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 new
+
+=head3 ARGUMENTS
+
+None.
+
+=head3 RETURNS
+
+A C<Pasteburn::Crypt::Hash> object.
+
+=head2 generate
+
+Method to generate hashed strings.
+
+=head3 ARGUMENTS
+
+=over
+
+=item string
+
+The plain text string for hashing.
+
+=back
+
+=head3 RETURNS
+
+The hashed string for storage into the database.
+
+=head2 validate
+
+Method to validate hashed strings with plaintext strings.
+
+=head3 ARGUMENTS
+
+=over
+
+=item hash
+
+The hashed string to use to validate.
+
+=item string
+
+The plain text string to validate.
+
+=back
+
+=head2 RETURNS
+
+1 or undef if the string matches the hash.
+
+=head1 AUTHOR
+
+Blaine Motsinger C<blaine@renderorange.com>
+
+=cut

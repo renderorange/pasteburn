277a1cc2fdcd9b82b62e64dc1cdcba54b62721ec
Blaine Motsinger <blaine@renderorange.com>
2020-08-24 23:03:54 -0500

    Add documentation for DB

--- lib/Pasteburn/DB.pm
+++ lib/Pasteburn/DB.pm
@@ -47,3 +47,59 @@ sub load {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+Pasteburn::DB - creates and connects to the database handle
+
+=head1 SYNOPSIS
+
+ use Pasteburn::DB ();
+
+ # as a class method
+ class_has _dbh => (
+     is      => 'rwp',
+     default => sub {
+         Pasteburn::DB::connect_db()
+     },
+ );
+
+ my $dbh = $class->_dbh;
+
+=head1 DESCRIPTION
+
+This module provides methods for other modules to connect to the database.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 connect_db
+
+=head3 ARGUMENTS
+
+None.
+
+=head3 RETURNS
+
+The DBI db database handle.
+
+=head2 load
+
+=head3 ARGUMENTS
+
+None.
+
+=head3 RETURNS
+
+The C<dsn>, C<user>, and C<password> strings, depending on the database type as defined in the config.
+
+=head3 CONFIGURATION
+
+C<connect_db> reads the database connection details from the C<.pasteburnrc> file, loaded through C<Pasteburn::Config>.
+
+=head1 AUTHOR
+
+Blaine Motsinger C<blaine@renderorange.com>
+
+=cut

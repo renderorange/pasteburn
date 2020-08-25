fb434400f78152eabe5f4e107fe6a6228bfed323
Blaine Motsinger <blaine@renderorange.com>
2020-08-24 23:24:17 -0500

    Add documentation for Storage

--- lib/Pasteburn/Crypt/Storage.pm
+++ lib/Pasteburn/Crypt/Storage.pm
@@ -55,3 +55,84 @@ sub decode {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+Pasteburn::Crypt::Storage - secure string hashing and validation
+
+=head1 SYNOPSIS
+
+ use Pasteburn::Crypt::Storage;
+
+ my $store = Pasteburn::Crypt::Storage->new(
+     passphrase => $passphrase,
+ );
+
+ my $encoded = $store->encode( secret => $secret );
+ my $decoded = $store->decode( secret => $encoded );
+
+=head1 DESCRIPTION
+
+This module provides secure encoding and decoding for storing strings.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 new
+
+=head3 ARGUMENTS
+
+=over
+
+=item passphrase
+
+The passphrase to use to encode and decode.
+
+=back
+
+=head3 RETURNS
+
+A C<Pasteburn::Crypt::Storage> object.
+
+=head2 encode
+
+Method to encode strings.
+
+=head3 ARGUMENTS
+
+=over
+
+=item secret
+
+The plain text string for encrypting.
+
+=back
+
+=head3 RETURNS
+
+The encoded string for storage into the database.
+
+=head2 decode
+
+Method to decode strings.
+
+=head3 ARGUMENTS
+
+=over
+
+=item secret
+
+The encoded string for decrypting.
+
+=back
+
+=head2 RETURNS
+
+The decoded string.
+
+=head1 AUTHOR
+
+Blaine Motsinger C<blaine@renderorange.com>
+
+=cut

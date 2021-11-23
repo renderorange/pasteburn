0fdaaab24bb254fa102ba415c70a8e40dbb47f62
Blaine Motsinger <blaine@renderorange.com>
2021-11-23 12:15:34 -0600

    Remove root template and redirect / to /secret
    For now, don't display the root template.
    
    TODO: update the root and route to display the create form.

--- lib/Pasteburn/Controller/Root.pm
+++ lib/Pasteburn/Controller/Root.pm
@@ -8,9 +8,7 @@ use Pasteburn::Model::Secrets ();
 our $VERSION = '0.001';
 
 get q{/} => sub {
-    my $template_params = { route => request->path, };
-
-    return template root => $template_params;
+    redirect '/secret';
 };
 
 1;

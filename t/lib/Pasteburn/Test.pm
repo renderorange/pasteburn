f1dad7d6dbb948187511695cefd590231831c986
Blaine Motsinger <blaine@renderorange.com>
2021-12-01 22:42:01 -0600

    Add db override and test db creation
    This commit adds a database override and creates a test database
    through Pasteburn::Test::import.
    
    The database override and test database creation can be skipped by
    passing skip_db => 1 into Pasteburn::Test when using.
    
    use Pasteburn::Test skip_db => 1;

--- t/lib/Pasteburn/Test.pm
+++ t/lib/Pasteburn/Test.pm
@@ -3,13 +3,15 @@ package Pasteburn::Test;
 use strict;
 use warnings;
 
+use File::Temp;
 use File::Path ();
+use Try::Tiny;
 
 use parent 'Test::More';
 
 our $VERSION = '0.005';
 
-our $tempdir = '';
+our ( $tempdir, $dbh );
 
 sub import {
     my $class = shift;
@@ -21,6 +23,15 @@ sub import {
     }
     elsif ( $args{skip_all} ) {
         $class->builder->plan( skip_all => $args{skip_all} );
+    }
+
+    $tempdir = File::Temp->newdir(
+        DIR     => $FindBin::RealBin,
+        CLEANUP => 0,
+    );
+
+    unless ( $args{skip_db} ) {
+        init_db();
     }
 
     Test::More->export_to_level(1);
@@ -61,13 +72,6 @@ sub write_config {
         @_,
     );
 
-    require File::Temp;
-
-    $tempdir = File::Temp->newdir(
-        DIR     => $FindBin::RealBin,
-        CLEANUP => 0,
-    );
-
     my $path = "$tempdir/.config/pasteburn";
     File::Path::make_path($path);
     my $rc = "$path/config.ini";
@@ -83,10 +87,52 @@ sub write_config {
     return $rc;
 }
 
+sub init_db {
+    my $db_path = $tempdir . '/test.sqlite3';
+    open( my $db_fh, '>', $db_path )
+        or die "open $db_path: $!\n";
+    close( $db_fh );
+
+    Test::More::note( "created test db - $db_path" );
+
+    override(
+        package => 'Pasteburn::DB',
+        name    => 'load',
+        subref  => sub {
+            return "dbi:SQLite:dbname=$db_path"
+        },
+    );
+
+    require Pasteburn::DB;
+    $dbh = Pasteburn::DB::connect_db();
+
+    my $schema;
+    my $module_path = Cwd::realpath(__FILE__);
+    $module_path =~ s/\w+\.pm//;
+    my $schema_path = $module_path . '/../../../db/schema/schema.sqlite';
+    open( my $schema_fh, '<', $schema_path )
+        or die "open $schema_path: $!\n";
+    while ( my $row = <$schema_fh> ) {
+        $schema .= $row;
+    }
+    close( $schema_fh );
+
+    my $result = try {
+        return $dbh->do( $schema );
+    }
+    catch {
+        die "insert schema failed: $_\n";
+    };
+
+    return;
+}
+
 END {
     if ( $tempdir ) {
-        Test::More::note( "cleaning up tempdir - $tempdir" );
-        unless ( File::Path::rmtree($tempdir) ) {
+        if ( File::Path::rmtree($tempdir) ) {
+            Test::More::note( "cleaned up tempdir - $tempdir" );
+        }
+        else {
             Test::More::diag( "rmtree: $!\n" );
         }
     }
